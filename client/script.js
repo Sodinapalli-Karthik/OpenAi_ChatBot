d29e0d95c9dad952803b4f5b0584890122c8532c
Sodinapalli-Karthik <93913520+Sodinapalli-Karthik@users.noreply.github.com>
2023-06-25 19:20:58 +0530

    deployed changes

--- client/script.js
+++ client/script.js
@@ -73,7 +73,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5001/', {
+    const response = await fetch('https://karty-chatbot.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
